484246b636f14e3efe90831732c34b9ebcbbc978
Kazim Kassam <nina@kazims-mbp.home>
2020-10-27 16:38:32 +0000

    deploy to heroku

---
+++ client/build/precache-manifest.9da7a043ac9c8265e279665317c0826f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a5ae990637f811339e21b676df543e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0beb272f4bb59b4eb0c8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4cb4f2c2c843950cfb8b",
+    "url": "/static/js/2.8a6f56c8.chunk.js"
+  },
+  {
+    "revision": "ad61e5a3703d8af976e070e9896b739a",
+    "url": "/static/js/2.8a6f56c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0beb272f4bb59b4eb0c8",
+    "url": "/static/js/main.eb28d7a7.chunk.js"
+  },
+  {
+    "revision": "0e60cda93199249b2927",
+    "url": "/static/js/runtime-main.2747de30.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "a56d285025f8799112910d2ff849f098",
+    "url": "/static/media/shopping-bag.a56d2850.svg"
+  }
+]);
